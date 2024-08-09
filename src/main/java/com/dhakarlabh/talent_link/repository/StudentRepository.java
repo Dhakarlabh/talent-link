0e043ffa33fb4352e5c2950f938d127381548268
Dhakarlabh <labhd45@gmail.com>
2024-08-09 20:50:32 +0530

    trying generics in java

---
+++ src/main/java/com/dhakarlabh/talent_link/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.dhakarlabh.talent_link.repository;
+
+import com.dhakarlabh.talent_link.models.Student;
+
+public class StudentRepository implements IRepository{
+
+    @Override
+    public <T> void save(T data) {
+
+    }
+}
