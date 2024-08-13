87f5d928153486b8458f24629fc34ef90fc41c28
Dhakarlabh <labhd45@gmail.com>
2024-08-13 23:10:17 +0530

    added code for mongo template

--- src/main/java/com/dhakarlabh/talent_link/repository/StudentRepository.java
+++ src/main/java/com/dhakarlabh/talent_link/repository/StudentRepository.java
@@ -1,34 +1,40 @@
 package com.dhakarlabh.talent_link.repository;
 
 import com.dhakarlabh.talent_link.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class StudentRepository implements IRepository<Student>{
 
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @Override
-    public Student save(Student entity) {
-        return new Student();
+    public Student save(Student studentInformation) {
+        return mongoTemplate.save(studentInformation);
     }
 
     @Override
     public Student get(String id) {
-        return null;
+        return mongoTemplate.findById(id, Student.class);
     }
 
     @Override
     public List<Student> getAll() {
-        return List.of();
+        return mongoTemplate.findAll(Student.class);
     }
 
     @Override
     public void delete(String id) {
-
+        mongoTemplate.remove(id);
     }
 
     @Override
-    public Student update(Student entity) {
-        return null;
+    public Student update(Student studentInformation) {
+        return mongoTemplate.save(studentInformation);
     }
 }
